7fd50bc8c6308344d192b1127ab60145e56759fa
ArchLeaders <archleadership28@gmail.com>
2024-01-14 14:59:39 -0800

    unroll 1 instead of 0 (should work the same?)

--- src/yaz0.cpp
+++ src/yaz0.cpp
@@ -166,7 +166,7 @@ static void Decompress(std::span<const u8> src, std::span<u8> dst) {
       if (base < &(*dst.begin()) || &(*dst_it) + length > &(*dst.end())) {
         throw std::invalid_argument("Copy is out of bounds");
       }
-#pragma GCC unroll 0
+#pragma GCC unroll 1
       for (size_t i = 0; i < length; ++i)
         *dst_it++ = base[i];
     }
